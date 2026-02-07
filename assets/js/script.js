e962f0fa60102cb38d3c1972e5da148ca24e755a
JARVIS243 <jarvisai0025@gmail.com>
2026-02-07 21:00:38 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,3 +208,12 @@ const observer = new IntersectionObserver(
 );
 
 mobileReveal.forEach(el => observer.observe(el));
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const progress = (scrollTop / scrollHeight) * 100;
+  document.getElementById("scroll-progress").style.width = progress + "%";
+});
